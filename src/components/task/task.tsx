f26e641ebd40462041efbffd306519836c0aa850
YKyrylenko <yaroslavkirilenko1@gmail.com>
2020-08-25 11:27:24 +0300

    changed variable name

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -59,15 +59,15 @@ const Task: FC<TaskProps> = ({
   const compare = (term: any): string => {
     let className = "task";
     if (term) {
-      let dt = new Date(term);
+      let taskTerm = new Date(term);
       let currentDate = new Date();
       currentDate.setHours(0, 0, 0, 0);
-      dt.setHours(0, 0, 0, 0);
-      if (compareAsc(currentDate, dt) > 0) {
+      taskTerm.setHours(0, 0, 0, 0);
+      if (compareAsc(currentDate, taskTerm) > 0) {
         className += " expiredTerm";
-      } else if (compareAsc(currentDate, dt) === 0) {
+      } else if (compareAsc(currentDate, taskTerm) === 0) {
         className += " equalTerm";
-      } else if (compareAsc(addDays(currentDate, 1), dt) === 0) {
+      } else if (compareAsc(addDays(currentDate, 1), taskTerm) === 0) {
         className += " oneDayLeftTerm";
       }
     }
